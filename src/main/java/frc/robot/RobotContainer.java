166a4f7d83c6aa747c979e80b0f09d8a8b52d361
Jet Pham <jetthomaspham@gmail.com>
2021-08-30 09:59:31 -0700

    tiny little changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,6 @@ import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.TurnShooterHoodToAngle;
 import frc.robot.subsystems.ShooterHood;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 /**
